bab7001622c2294c885e864f299d9ddbce4db85f
Rian dos Passos <riandpf2002@gmail.com>
2022-03-20 17:56:41 -0300

    add login

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 def Home(request):
     return render(request, 'home.html')
@@ -19,3 +20,20 @@ def Store(request): # Inserção dos dados dos usuário no banco
         data['class'] = 'alert-success'
 
     return render(request, 'create.html', data)
+
+def Painel(request): #Formulário do painel de login
+    return render(request, 'painel.html')
+
+def Login(request): #Processar o login
+    data = {}
+    user = authenticate(username=request.POST['user'], password=request.POST['password'])
+    if user is not None:
+        login(request,user)
+        return redirect('/dashboard/')
+    else:
+        data['msg'] = 'Usuário e ou senha inválidos!'
+        data['class'] = 'alert-danger'
+        return render(request, 'painel.html', data)
+
+def dashboard(request): #Página inicial do dashboard
+    return render(request, 'dashboard/home.html')
--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import Home, Create, Store
+from app.views import Home, Create, Store, Painel, Login, dashboard
 
 urlpatterns = [
     path('', Home),
     path('create/', Create),
     path('store/', Store),
+    path('painel/', Painel),
+    path('login/', Login),
+    path('dashboard/', dashboard),
 ]
